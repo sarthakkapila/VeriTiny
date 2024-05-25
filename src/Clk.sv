f06a106cacbc8476f0ed1f41d5d91fc73ff7360d
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-05-26 02:47:06 +0530

    added IFU

--- src/Clk.sv
+++ src/Clk.sv
@@ -1,3 +1,7 @@
+//////////////////////////////////////////////////////////////////////////////
+//                          Generates clock signal                          //
+//////////////////////////////////////////////////////////////////////////////
+
 module CLK (
     input logic clk_in,                     // Input clock signal
     output logic clk_out,                   // Output clock signal
@@ -5,7 +9,7 @@ module CLK (
 
 reg [31:0] count;                           // Counter to divide the clock frequency for lower clk_out
                                             // Dividing the clock frequency into smaller parts using a counter allows you to provide different clock speeds to different parts of the digital system based on their requirements.
-                                            
+
 always @(posedge clk_in) begin
     if (count == 50000000) begin            // 0.5 second for 100 MHz
         clk_out <= ~clk_out;                // output clock
--- src/IFU.sv
+++ src/IFU.sv
@@ -1,4 +1,12 @@
 //////////////////////////////////////////////////////////////////////////////
 //             Fetching and prepare instruction for execution               //
-/////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
+module InstructionFetchUnit (
+    input  logic          clk,             // Clock signal
+    input  logic [31:0]   pc,              // Program counter
+    input  logic          branch_taken,    // Control signal indicating a branch instruction is taken
+    input  logic [31:0]   branch_target,   // Target address for branch instruction
+    input  logic          stall,           // Control signal to stall instruction fetching
+    output logic [31:0]   instruction      // Fetched instruction
+);
